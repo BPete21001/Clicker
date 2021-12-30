c15cc38ef01df6c5c85f081b20f70aa9f3be7084
Benjamin <petebs10@gmail.com>
2021-12-30 16:03:09 -0600

    Addition of main game script

---
+++ AppSource/Game.py
@@ -0,0 +1,20 @@
+#Import necessary modules
+import pygame 
+from random import randint
+
+#Start pygame
+pygame.init()
+running = True
+
+while running 
+	
+	#Check for inputs 
+	for event in pygame.event.get():
+		#Exit the loop if the game is quit
+		if event.type == pygame.QUIT: 
+			running = False
+
+
+
+#End Pygame
+pygame.quit()
