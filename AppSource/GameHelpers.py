8ad22b6addded1caca26435107c44ce9112a7ad9
Benjamin Petersen <petebs10@gmail.com>
2022-01-10 14:50:30 -0600

    Add method to determine if click is in circle

--- AppSource/GameHelpers.py
+++ AppSource/GameHelpers.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from Classes import *
 from random import randint
 
@@ -16,3 +17,22 @@ def randomizeCirclePosition(circle):
 	width, height = pygame.display.get_surface().get_size()
 	circle.setXPos((int)(width * 0.01 * randint(15, 85))) #randomize XPosition to a value between 15% and 5% of the screen's width
 	circle.setYPos((int)(height * 0.01 * randint(15, 85))) #randomize YPosition to a value between 15% and 85% of the screen's height
+
+def isOverCircle(circle):
+	mouseX, mouseY = pygame.mouse.get_pos()
+
+	#calculate how far away from the center of the circle both of the mouse positions are 
+	xDif = abs(mouseX - circle.getXPos())
+	yDif = abs(mouseY - circle.getYPos())
+
+	#use pythagorean theorem to calculate how far from the center of the circle the the click was
+	distanceAway = math.sqrt((xDif ** 2) + (yDif ** 2))
+
+	#return true if the click is within the range
+	if(circle.getRadius() >= distanceAway):
+		return True
+	else:
+		return False
+
+
+
